654d5efeea5cdc4e7f6fbbd2000d1fd57ab39a25
Mohamed Hassan <mohamed>
2021-06-25 13:03:23 +0200

    any

--- index.js
+++
@@ -1,8 +0,0 @@
-function training(str) {
-  let counter = 0
-  for (let i = 0; i < str.length; i++) if (str.substr(i, 6) == "potato") counter++
-  console.log(counter)
-}
-training("potato")
-training("potatopotatokiuhkjhpotatoljkhpotato")
-training("potatoapplepotato")
